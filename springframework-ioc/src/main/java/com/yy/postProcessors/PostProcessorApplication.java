23a21c6ab02361280636fc844f45c82ffebd16c5
yuyue <yuyue@odianyun.com>
2019-04-23 16:55:50 +0800

    BeanPostProcessor

--- springframework-ioc/src/main/java/com/yy/postProcessors/PostProcessorApplication.java
+++ springframework-ioc/src/main/java/com/yy/postProcessors/PostProcessorApplication.java
@@ -7,9 +7,12 @@ public class PostProcessorApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("postProcessor.xml");
+        // 验证init方法与BeanPostProcessor接口的顺序
         ExampleBean1 bean1 = context.getBean("bean1", ExampleBean1.class);
+        // BeanPostProcessor + JDK动态代理
         ICustomBean iCustomBean = (ICustomBean) context.getBean("bean2");
         iCustomBean.printName();
+        // ???
         ExampleBean1 bean3 = CustomBeanFactoryPostProcessor.getBean("bean1", ExampleBean1.class);
     }
 
